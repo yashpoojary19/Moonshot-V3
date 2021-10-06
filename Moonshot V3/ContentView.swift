b8a3e0003c6aa9263fa4ff73b6f3a8780d954166
Yash Poojary <yashspoojary245@gmail.com>
2021-10-06 13:09:49 +0530

    Initial Commit

---
+++ Moonshot V3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Moonshot V3
+//
+//  Created by Yash Poojary on 06/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Moonshot V3/Moonshot_V3App.swift
@@ -0,0 +1,17 @@
+//
+//  Moonshot_V3App.swift
+//  Moonshot V3
+//
+//  Created by Yash Poojary on 06/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Moonshot_V3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
